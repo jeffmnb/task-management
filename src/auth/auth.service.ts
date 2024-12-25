260203eac19b2af847083bb57ad2cdfcefb4eed6
jeffmnb <jefeson.nborda@gmail.com>
2024-12-25 09:58:22 -0300

    wip:user routes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { UsersRepository } from './users.repository';
@@ -23,6 +27,11 @@ export class AuthService {
       email,
       password,
     });
-    await this.usersRepository.save(user);
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      if (error.code == '23505')
+        throw new ConflictException('email already exists');
+    }
   }
 }
